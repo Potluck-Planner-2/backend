f0f4e9a43ea1d9c0ee7f0d04f053f5d2f0111e93
Victoria  Topham <topham.victoria@gmail.com>
2020-05-23 10:38:38 -0700

    basic server is up and running

---
+++ api/server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const helmet = require('helmet');
+
+const server = express();
+
+server.get('/', (req, res) => {
+    res.status(200).json({message: "Server is up and running"});
+});
+
+module.exports = server;
