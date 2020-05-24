2f093f0b1be1d5c4dcd43379006214825d42852c
Victoria  Topham <topham.victoria@gmail.com>
2020-05-24 11:06:24 -0700

    ah, i think it was that i needed to reseed the data

--- data/seeds/00-users.js
+++ data/seeds/00-users.js
@@ -17,12 +17,12 @@ exports.seed = function(knex) {
       // Inserts seed entries
       return knex('users').insert([
         {id: 1, username: 'nobody', first_name: 'Nobody ', last_name: ' ', password: nobodyPass},
-        {id: 2, username: 'patrick????', first_name: 'Patrick', last_name: 'Harl', password: patrickPass},
+        {id: 2, username: 'patrick', first_name: 'Patrick', last_name: 'Harl', password: patrickPass},
         {id: 3, username: 'barbara', first_name: 'Barbara', last_name: 'Moore', password: barbaraPass},
         {id: 4, username: 'cristina', first_name: 'Cristina', last_name: 'Altreche', password: cristinaPass},
         {id: 5, username: 'victoria', first_name: 'Victoria', last_name: 'Topham', password: victoriaPass},
-        {id: 6, username: 'lisa', first_name: 'Lisa', last_name: 'Maskovich', password: lisaPass},
-        {id: 7, username: 'vic2', first_name: 'vic2', last_name: 'vic2', password: vic2}
+        {id: 6, username: 'lisa', first_name: 'Lisa', last_name: 'Maskovich', password: lisaPass}
+        
       ]);
     });
 };
